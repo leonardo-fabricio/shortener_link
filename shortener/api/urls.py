6b528cd69d702d9f8cde7e151c0bad367a4ff764
leonardo-fabricio <leonnardofabbricio@gmail.com>
2022-03-28 12:02:01 -0300

    correcao de bug na rota

--- shortener/api/urls.py
+++ shortener/api/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .viewsets import *
 
 urlpatterns = [
-    path('encutar-link/', LinkCreateViewSet.as_view()),
+    path('encurtar-link/', LinkCreateViewSet.as_view()),
 ]
